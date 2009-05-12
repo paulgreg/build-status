e3cf0df2195bd0625c474b2d541afdffe27e6b42
paulgreg <paulgreg@3cc63e16-3ef8-11de-a8f1-5f41fc3ad33f>
2009-05-12 20:01:11 +0000

    initial version 4

---
+++ build-status.py
@@ -0,0 +1,104 @@
+import cgi
+import datetime
+
+from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import db
+
+import os
+from google.appengine.ext.webapp import template
+
+
+class Build(db.Model):
+  app_name = db.StringProperty(multiline=False)
+  has_passed = db.BooleanProperty()
+  last_update = db.DateTimeProperty(auto_now_add=True)  
+
+
+def getBuild(app_name):
+  builds = Build.gql("WHERE app_name = :1", app_name)
+  if builds.count():
+    build = builds.get()
+  else:
+    build = Build()
+    build.app_name = app_name
+    build.has_passed = False
+    build.put()
+  return build
+
+
+def gotoHome(self):
+  self.response.out.write(template.render(os.path.join(os.path.dirname(__file__), 'home.html'), { } ))
+
+def render(self, app_name, page):
+  build = getBuild(app_name)
+
+  if build.has_passed:
+    build.status = "pass"
+    build.passed = "true"
+  else:
+    build.status = "fail"
+    build.passed = "false"
+
+  build.current_time = datetime.datetime.now()
+
+  path = os.path.join(os.path.dirname(__file__), page)
+  self.response.out.write(template.render(path,	{ 'build': build } ))
+
+
+class MainPage(webapp.RequestHandler):
+  def get(self):
+    app_name = self.request.get('app_name')
+
+    if not app_name:
+      gotoHome(self)
+    else:
+      render(self, app_name, 'action.html');
+
+
+def updatePage(self, status):
+  app_name = self.request.get('app_name')
+
+  if not app_name:
+    gotoHome(self)
+  else:
+    build = getBuild(app_name)
+    build.has_passed = status
+    build.last_update = datetime.datetime.now()
+    build.put()
+    self.redirect('/?app_name=' + app_name)
+
+
+class UpdateToPassedPage(webapp.RequestHandler):
+  def get(self):
+    updatePage(self, True)
+
+
+class UpdateToFailedPage(webapp.RequestHandler):
+  def get(self):
+    updatePage(self, False)
+
+
+class GetJsonState(webapp.RequestHandler):
+  def get(self):
+    app_name = self.request.get('app_name')
+
+    if not app_name:
+      self.response.out.write('Parameter "app_name" is mandatory !')
+    else:
+      self.response.headers['Content-Type'] = 'application/json'
+      render(self, app_name, 'state.js')
+
+
+application = webapp.WSGIApplication(
+                                     [('/', MainPage),
+                                     ('/pass', UpdateToPassedPage),
+                                     ('/fail', UpdateToFailedPage),
+                                     ('/state', GetJsonState)])
+
+def main():
+  run_wsgi_app(application)
+
+if __name__ == "__main__":
+  main()
